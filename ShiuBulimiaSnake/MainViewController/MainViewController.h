4dd6a794a462b220c4799ebfc09d2cf60d24d881
Allen_shiu <pt7922310@gmail.com>
2016-01-19 14:21:18 +0800

    新增 MainViewController 。

--- ShiuBulimiaSnake/MainViewController/MainViewController.h
+++ ShiuBulimiaSnake/MainViewController/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  ShiuBulimiaSnake
 //
 //  Created by 許佳豪 on 2016/1/19.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
---
+++ ShiuBulimiaSnake/MainViewController/MainViewController.m
@@ -0,0 +1,22 @@
+//
+//  MainViewController.m
+//  ShiuBulimiaSnake
+//
+//  Created by 許佳豪 on 2016/1/19.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
