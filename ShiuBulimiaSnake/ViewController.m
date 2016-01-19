70ba39c9f440dca17556d2c9342980e68277213c
Allen_shiu <pt7922310@gmail.com>
2016-01-19 14:06:45 +0800

    Initial Commit

---
+++ ShiuBulimiaSnake/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShiuBulimiaSnake
+//
+//  Created by 許佳豪 on 2016/1/19.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ShiuBulimiaSnake/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShiuBulimiaSnake
+//
+//  Created by 許佳豪 on 2016/1/19.
+//  Copyright © 2016年 許佳豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
